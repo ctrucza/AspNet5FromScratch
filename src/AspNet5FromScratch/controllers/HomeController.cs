8449afbb81fbed3dc70aaf9b124ed255509cb147
Csaba Trucza <csaba.trucza@gmail.com>
2016-03-24 14:28:02 +0200

    First page

---
+++ src/AspNet5FromScratch/controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace AspNet5FromScratch.controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
